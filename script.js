d6acac5e2c3702f5962fea4fedf525fdff397232
Joon Sunn <joonsunn.foo@gmail.com>
2022-01-25 20:23:27 +0800

    refactor to UI version

--- script.js
+++ script.js
@@ -62,41 +62,84 @@ function cleanPlayerInput(){
     return playerSelection
 };
 
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    let matchLimit = 5
+// function game(){
+//     let playerScore = 0;
+//     let computerScore = 0;
+//     let matchLimit = 5
     
-    for (let i = 1; i <= matchLimit; i++){
-        let computerSelection = computerPlay();
+//     for (let i = 1; i <= matchLimit; i++){
+//         let computerSelection = computerPlay();
         
 
-        let playerSelection = cleanPlayerInput();
-        console.log(`Computer selected ${computerSelection}`);
-        console.log(`Player selected ${playerSelection}`);
+//         let playerSelection = cleanPlayerInput();
+//         console.log(`Computer selected ${computerSelection}`);
+//         console.log(`Player selected ${playerSelection}`);
         
-        let result = playRound(playerSelection, computerSelection);
-        console.log(result);
+//         let result = playRound(playerSelection, computerSelection);
+//         console.log(result);
 
-        if (result.includes("win")){
-            playerScore++;
-            console.log(`Player wins Round ${i}!`);
-        }
-        else if (result.includes("lose")){
-            computerScore++;
-            console.log(`Computer wins Round ${i}!`);
+//         if (result.includes("win")){
+//             playerScore++;
+//             console.log(`Player wins Round ${i}!`);
+//         }
+//         else if (result.includes("lose")){
+//             computerScore++;
+//             console.log(`Computer wins Round ${i}!`);
+//         }
+//         else{
+//             matchLimit++;
+//         }
+//         console.log(`Score after Round ${i}: Player ${playerScore} - Computer ${computerScore}`);
+
+//         if (playerScore == 3 || computerScore == 3){
+//             break;
+//         }
+//     }
+
+//     console.log(playerScore>computerScore ? "Player won the match!":"Computer won the match!");
+// }
+
+
+
+const buttons = document.querySelectorAll("button");
+const result = document.querySelector("#result");
+const score = document.querySelector("#score");
+let playerScore = 0;
+let computerScore = 0;
+
+buttons.forEach(button => button.addEventListener('click', () => {
+    
+    let playerSelection = this.document.activeElement.id;
+    let computerSelection = computerPlay();
+    console.log(`playerSelection: ${playerSelection}, computerSelection: ${computerSelection}`);
+    let resultText = playRound(playerSelection, computerSelection);
+    // console.log(result);
+    result.textContent = resultText;
+    if (resultText.includes("win")){
+                    playerScore++;
+                }
+                else if (resultText.includes("lose")){
+                    computerScore++;
+                }
+    score.textContent = `Current score: Player ${playerScore} - Computer ${computerScore}`;
+
+    if(playerScore == 3 || computerScore == 3){
+        score.textContent = `FINAL score: Player ${playerScore} - Computer ${computerScore}`;
+        
+        if (playerScore>computerScore){
+            result.textContent = `End of Match! Player wins the match! Make a new selection to start a new match!`;
         }
         else{
-            matchLimit++;
+            result.textContent = `End of Match! Computer wins the match! Make a new selection to start a new match!`;
         }
-        console.log(`Score after Round ${i}: Player ${playerScore} - Computer ${computerScore}`);
-
-        if (playerScore == 3 || computerScore == 3){
-            break;
-        }
+        playerScore = 0;
+        computerScore = 0;
     }
 
-    console.log(playerScore>computerScore ? "Player won the match!":"Computer won the match!");
-}
 
-game();
+}));
+
+
+
+
+
